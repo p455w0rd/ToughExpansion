a0c969d1273a19975c0683228921fb5cb0ff45f4
p455w0rd <p455w0rd@gmail.com>
2017-08-02 14:33:34 -0400

    Fix tooltip crash on temp regulator block

--- src/main/java/p455w0rd/tanaddons/blocks/BlockBase.java
+++ src/main/java/p455w0rd/tanaddons/blocks/BlockBase.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import org.lwjgl.input.Keyboard;
-
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -20,7 +16,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -46,15 +41,9 @@ public class BlockBase extends BlockContainer {
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced) {
-		KeyBinding sneak = Minecraft.getMinecraft().gameSettings.keyBindSneak;
-		if (playerIn.isSneaking() || Keyboard.isKeyDown(sneak.getKeyCode())) {
-			tooltip.add("");
-			tooltip.add(I18n.format("tooltip.tanaddons.redstone_rclick.desc"));
-			tooltip.add(I18n.format("tooltip.tanaddons.redstone_rclick.desc2"));
-		}
-		else {
-			tooltip.add(TextFormatting.ITALIC + "" + TextFormatting.AQUA + "" + I18n.format("tooltip.tanaddons.holdshift", sneak.getDisplayName()));
-		}
+		tooltip.add("");
+		tooltip.add(I18n.format("tooltip.tanaddons.redstone_rclick.desc"));
+		tooltip.add(I18n.format("tooltip.tanaddons.redstone_rclick.desc2"));
 	}
 
 	@SideOnly(Side.CLIENT)
