62de9970168d9ea7a5c70d60d4598bfe15ea9f30
grundyboy34 <grundyboy34@gmail.com>
2017-10-09 17:48:36 -0400

    Corrected required Mod-Ids

--- src/main/java/p455w0rd/tanaddons/init/ModGlobals.java
+++ src/main/java/p455w0rd/tanaddons/init/ModGlobals.java
@@ -2,9 +2,9 @@ package p455w0rd.tanaddons.init;
 
 public class ModGlobals {
 
-	public static final String MODID_BAUBLES = "baubles";
-	public static final String MODID_TAN = "toughasnails";
-	public static final String MODID_PWLIB = "p455w0rdslib";
+    public static final String MODID_BAUBLES = "Baubles";
+    public static final String MODID_TAN = "ToughAsNails";
+    public static final String MODID_PWLIB = "p455w0rdslib";
 
 	public static final String MODID = "tanaddons";
 	public static final String VERSION = "3.1.4";
